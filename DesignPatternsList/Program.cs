c83a0bbe938299bb2612f0eb68cfbb3be1940ee5
Mashashi Nakamura <mashashi.nakamura@gmail.com>
2015-10-27 10:40:29 +0100

    Added Template Method.

---
+++ DesignPatternsList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Commands;
+
+namespace DesignPatternsList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Default main");
+            System.Console.ReadLine();
+        }
+    }
+
+}
---
+++ DesignPatternsList/TemplateMethod/TemplateMethod.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commands
+{
+    abstract class Console
+    {
+        public abstract String getRoot();
+        public void printStatus() {
+            System.Console.WriteLine(getRoot() + ">");
+        }
+    }
+
+    class Cmd : Console{
+
+        public override String getRoot() {
+               return "C:\\";
+        }
+
+    }
+
+    class Terminal : Console
+    {
+
+        public override String getRoot()
+        {
+            return "\\";
+        }
+
+    }
+
+    class Client
+    {
+
+        public static void Main(String[] arg)
+        {
+            Commands.Console c = new Cmd();
+            System.Console.WriteLine(c.getRoot());
+            System.Console.ReadLine();
+        }
+
+    }
+
+}
